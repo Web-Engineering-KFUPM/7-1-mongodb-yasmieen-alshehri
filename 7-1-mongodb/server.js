d7465316d5e424869a87d8228d282b865f45030c
Yasmeen Alshehri <s202271660@kfupm.edu.sa>
2026-04-13 13:57:27 +0300

    TODO 2 define schema

--- 7-1-mongodb/server.js
+++ 7-1-mongodb/server.js
@@ -196,7 +196,13 @@ mongoose.connect("mongodb+srv://yasmeen:%40Ynda4321@cluster0.lnjvkzj.mongodb.net
   .catch(err => console.log(err));
 
 // define schema
+const studentSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  major: String
+});
 
+const Student = mongoose.model("Student", studentSchema);
 
 // create document
 
