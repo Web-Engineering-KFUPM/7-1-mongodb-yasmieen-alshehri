9847b23dcfb295bde4ca3002c72660d4b1ef7b68
Yasmeen Alshehri <s202271660@kfupm.edu.sa>
2026-04-13 14:01:49 +0300

    TODO-3 Create Document

--- 7-1-mongodb/server.js
+++ 7-1-mongodb/server.js
@@ -205,7 +205,15 @@ const studentSchema = new mongoose.Schema({
 const Student = mongoose.model("Student", studentSchema);
 
 // create document
+async function createStudents() {
+  await Student.insertMany([
+    { name: "Ali", age: 21, major: "CS" },
+    { name: "Sara", age: 23, major: "SE" }
+  ]);
+  console.log("✅ Inserted");
+}
 
+createStudents()
 
 // read document
 
